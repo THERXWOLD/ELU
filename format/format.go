c33b5f83fa36409a49a01efb9840e27e9a27cdb6
Naru K <k@narukoshin.me>
2026-07-13 18:17:49 +0300

    fix(format): silently deleting tmp file instead of error handling

--- format/format.go
+++ format/format.go
@@ -33,12 +33,8 @@ func Bytes(path string, src []byte) ([]byte, error) {
 
 // Path formats the file at path in place. Overwrites the original. It creates a temp file first, then renames it to the original file.
 func Path(path string) error {
-	// removing .tmp file in case of failure
-	err := os.Remove(path + ".tmp")
-	if err != nil {
-		return err
-	}
-
+	// silently remove the temp file if it exists
+	_ = os.Remove(path + ".tmp")
 	b, err := os.ReadFile(path)
 	if err != nil {
 		return err
